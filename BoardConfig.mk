b54cabdf894c9e0ff612bf85dcc3007c8068d982
Hamster Tian <th0928@126.com>
2013-12-22 21:23:43 +0800

    First commit for sony hayabusa CyanogenMod 11

---
+++ BoardConfig.mk
@@ -0,0 +1,33 @@
+#inherit from the common blue definitions
+-include device/sony/blue-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH += device/sony/hayabusa/include
+
+TARGET_KERNEL_CONFIG := cm_blue_hayabusa_defconfig
+
+# Partition information
+BOARD_VOLD_MAX_PARTITIONS := 18
+
+# the following two sizes are generous guesses
+# since these partitions are not visible
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/hayabusa/bluetooth
+
+TARGET_LS_USE_ALS_NODE := true
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+TARGET_OTA_ASSERT_DEVICE := lt29i,hayabusa,LT29i,mint,lt30p,LT30p
+
+BOARD_HARDWARE_CLASS := device/sony/hayabusa/cmhw
+
+# inherit from the proprietary version
+-include vendor/sony/hayabusa/BoardConfigVendor.mk
---
+++ cm.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/sony/hayabusa/full_hayabusa.mk)
+
+# Inherit CM common GSM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Enhanced NFC
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT29i BUILD_FINGERPRINT=Sony/LT29i_1266-5813/LT29i:4.1.2/9.1.B.1.67/vLt_tw:user/release-keys PRIVATE_BUILD_DESC="LT29i-user 4.1.2 9.1.B.1.67 c_t_tw test-keys"
+
+PRODUCT_NAME := cm_hayabusa
+PRODUCT_DEVICE := hayabusa
